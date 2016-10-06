5432081fc70a5ef991abd0b1af5e5d42602e0f32
ylrad <ylrad@ylrad-PC>
2016-10-05 23:20:11 -0300

    Este e meu terceiro commit !!!

--- NossoProjetoTest/index.php
+++ NossoProjetoTest/index.php
@@ -10,8 +10,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <p>esta e minha auteracao.. !!!  :)</p> 
     </body>
 </html>
